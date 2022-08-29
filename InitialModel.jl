af0c0f7eb9d51dc08043ad34abe3c463b1f22af5
thearonn <thea.ronn@hotmail.com>
2022-08-29 16:37:01 +0200

    Juhuu

--- InitialModel.jl
+++ InitialModel.jl
@@ -2,7 +2,6 @@ using JuMP, GLPK
 
 model = Model(GLPK.Optimizer)
 
-T = collect(1:52)
 P = [1,2,3,4,5,6] #two priorities, 3 channels #[DR1 kernen, DR1 perspektiv, DR2-K, DR2-P, DR3-K, DR3-P]
 C = 3 #number of channels
 M = 0
@@ -10,12 +9,18 @@ M = 0
 L = [-1,0,1]
 Q = [-2,-1]
 
+time_start = abs(minimum(Q)) + 1
+time_end = abs(minimum(Q)) + 10
+timesteps = abs(minimum(Q)) + 10 + maximum(L)
+
+T = collect(1:timesteps)
+
 I = zeros(length(T), C)
 I[:,1] = ones(length(T))*(120*7 + M)
 I[:,2] = ones(length(T))*(15*7 + M)
 I[:,3] = ones(length(T))*(7*7 + M)
 
-S = [12,13,2,29,0,0]
+S = [4,1,0,1,1,1] 
 w = [100,80,50,50,60,10]
 H = ones(length(T))*(100 + M)
 u = zeros(length(L),length(P), C)
@@ -28,11 +33,11 @@ u[:,6,:] = [0 0 0; 0 47 118; 0 0 0]
 
 @variable(model, x[1:length(T), 1:length(P)] >= 0, Int)
 
-@objective(model, Max, sum(x[t,p] for t = 2:length(T) for p = 1:length(P)))
-
-@constraint(model, [t=2:(length(T)-1), l in 1:length(L), c = 1:C], sum(u[l,p,c] * x[t,p] for p = 1:length(P)) <= I[t+L[l],c])
+@objective(model, Max, sum(x[t,p] for t = time_start:time_end for p = 1:length(P)))
+@constraint(model, [t=time_start:time_end, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = 1:length(L) for p = 1:length(P)) <= I[t,c])
+#@constraint(model, [t=time_start:time_end, l in 1:length(L), c = 1:C], sum(u[l,p,c] * x[t,p] for p = 1:length(P)) <= I[t+L[l],c])
 @constraint(model, [p=1:length(P)], sum(x[t,p] for t = 1:length(T)) >= S[p])
-@constraint(model, [t=3:length(T), q = 1:length(Q)], sum(w[p]*x[t,p] for p=1:length(P)) <= H[t+Q[q]])
+@constraint(model, [t=time_start:time_end, q = 1:length(Q)], sum(w[p]*x[t,p] for p=1:length(P)) <= H[t+Q[q]])
 
 JuMP.optimize!(model)
 println("Objective: ", objective_value(model))
