bf5f58cb50eae387d8830954f55043bfdf88f11b
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-08-30 13:59:42 +0200

    changes og kirser

--- InitialModel.jl
+++ InitialModel.jl
@@ -7,12 +7,13 @@ C = 3 #number of channels
 M = 0
 
 L = [-1,0,1]
+L_boundary = [1]
 Q = [-3,-2]
 
 timeperiod = 10
 
-time_start = abs(minimum(Q)) + 1
-time_end = abs(minimum(Q)) + timeperiod
+start = abs(minimum(Q)) + 1
+stop = abs(minimum(Q)) + timeperiod
 timesteps = abs(minimum(Q)) + timeperiod + maximum(L)
 
 T = collect(1:timesteps)
@@ -22,7 +23,7 @@ I[:,1] = ones(length(T)) * (120 * 7 + M)
 I[:,2] = ones(length(T)) * (15 * 7 + M)
 I[:,3] = ones(length(T)) * (7 * 7 + M)
 
-S = [1,5,2,0,3,0] #zeros(6) #[1,1,0,1,1,1] 
+S = [1,2,2,0,3,0] #zeros(6) #[1,1,0,1,1,1] 
 w = [100,80,50,50,60,10] #weekly cost
 H = ones(length(T)) * (100 + M)
 u = zeros(length(L), length(P), C)
@@ -34,30 +35,47 @@ u[:,:,3] = [0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0]
 @variable(model, x[1:length(T), 1:length(P)] >= 0, Int)
 @variable(model, f[1:length(P)] >= 0, Int)
 
-@objective(model, Max, sum(x[t,p] for t = time_start:time_end for p = 1:length(P)) - sum(f[p] for p = 1:length(P)))
+@objective(model, Max, sum(x[t,p] for t = start:stop for p = 1:length(P)) - sum(f[p] for p = 1:length(P)))
 
-@constraint(model, [t = 1:(time_start - 1)], sum(x[t,p] for p = 1:length(P)) == 0)
-@constraint(model, [t = (time_end + 1):timesteps], sum(x[t,p] for p = 1:length(P)) == 0)
+@constraint(model, [t = 1:(start - 1)], sum(x[t,p] for p = 1:length(P)) == 0)
+@constraint(model, [t = (stop + 1):timesteps], sum(x[t,p] for p = 1:length(P)) == 0)
 
-@constraint(model, [t=time_start:time_end, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = 1:length(L) for p = 1:length(P)) <= I[t,c])
-@constraint(model, [t=1:(time_end - time_start)], sum(w[p] * x[t-Q[q],p] for p=1:length(P) for q = 1:length(Q)) <= H[t])
+# Inventory from t = start:stop
+@constraint(model, [t=start:stop, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = 1:length(L) for p = 1:length(P)) <= I[t,c])
+@constraint(model, [t=(stop+1):timesteps, c = 1:C], sum(u[l,p,c] * x[t-L_boundary[l],p] for l = 1:length(L_boundary) for p = 1:length(P)) <= I[t,c])
 
+@constraint(model, [t=1:(stop - start)], sum(w[p] * x[t-Q[q],p] for p=1:length(P) for q = 1:length(Q)) <= H[t])
 
-@constraint(model, [p=1:length(P)], sum(x[t,p] for t = time_start:time_end) >= S[p] - f[p])
-
+@constraint(model, [p=1:length(P)], sum(x[t,p] for t = start:stop) >= S[p] - f[p])
 
 JuMP.optimize!(model)
 println("Objective: ", objective_value(model))
+sol = zeros(Int, length(T), length(P))
 for t = 1:length(T)
     for p = 1:length(P)
         if JuMP.value(x[t,p]) > 0.5
             println("At time ", t, " we have prioritet ", p, " with value: ", JuMP.value(x[t,p]))
+            sol[t,p] = JuMP.value(x[t,p])
         end
     end
 end
 
 for p = 1:length(P)
-    if JuMP.value(f[p]) > 0.5
+    if JuMP.value(f[p]) > 0
         println("Penalty for priority ", p , " with value: ", JuMP.value(f[p]))
     end
 end
+
+# Write solution
+function writeSolution(filename, sol)
+    outFile = open(filename, "w")
+    for t = 1:timesteps
+        write(outFile,join(sol[t,:]," ")*"\n")
+    end
+    close(outFile)
+end
+
+writeSolution("solution.txt", sol)
+
+
+
