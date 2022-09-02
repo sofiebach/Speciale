6e4c33ea99eb380d38eb7441dd7d9bfc2aab019f
thearonn <thea.ronn@hotmail.com>
2022-09-02 12:13:15 +0200

    Validation functions

--- ValidateSolution.jl
+++ ValidateSolution.jl
@@ -16,50 +16,59 @@ end
 x = readSolution("output/solution.txt")
 
 # Overview of inventory
-inventory_check = zeros(C, T)
-inventory_dif = zeros(C, T)
-for t = start:stop
-    for p = 1:P
-        if x[t,p] > 0
-            l_idx = 1
-            for l in L
-                for c = 1:C
-                    grp = u[l_idx,p,c] * x[t,p]
-                    inventory_check[c, t+l] += grp
-                    inventory_dif[c, t+l] = I[t+l,c] - inventory_check[c, t+l]
-                    if inventory_check[c, t+l] > I[t+l,c]
-                        println("p: ", p)
-                        println("c: ", c)
-                        println("t: ", t)
-                        println("l: ", l)
+function checkSolution(filename)
+    x = readSolution(filename)
+    inventory_check = zeros(C, T)
+    inventory_used = zeros(C, T)
+    for t = start:stop
+        for p = 1:P
+            if x[t,p] > 0
+                l_idx = 1
+                for l in L
+                    for c = 1:C
+                        grp = u[l_idx,p,c] * x[t,p]
+                        inventory_check[c, t+l] += grp
+                        inventory_used[c, t+l] = 1 - (I[t+l,c] - inventory_check[c, t+l])/I[t+l,c]
+                        if inventory_check[c, t+l] > I[t+l,c]
+                            println("Inventory constraint exceeded!")
+                            println("p: ", p)
+                            println("c: ", c)
+                            println("t: ", t)
+                            println("l: ", l)
+                        end
                     end
-                end
-                l_idx += 1
-            end   
+                    l_idx += 1
+                end   
+            end
         end
     end
-end
 
-# Overview of staffing
-staffing_check = zeros(T)
-staffing_dif = zeros(T)
-for t = start:stop
-    for p = 1:P
-        if x[t,p] > 0
-            q_idx = 1
-            work = w[p]
-            for q in Q
-                staffing_check[t+q] += work
-                staffing_dif[t+q] = H[t+q] - staffing_check[t+q]
-                if staffing_check[t+q] > H[t+q]
-                    println("t: ", t)
-                    println("p: ", p)
-                    println("q: ", q)
-                end
-                q_idx += 1
-            end   
+    # Overview of staffing
+    staffing_check = zeros(T)
+    staffing_used = zeros(T)
+    for t = start:stop
+        for p = 1:P
+            if x[t,p] > 0
+                q_idx = 1
+                work = w[p]
+                for q in Q
+                    staffing_check[t+q] += work
+                    staffing_used[t+q] = 1 - (H[t+q] - staffing_check[t+q])/H[t+q]
+                    if staffing_check[t+q] > H[t+q]
+                        println("Staff constraint exceeded!")
+                        println("t: ", t)
+                        println("p: ", p)
+                        println("q: ", q)
+                    end
+                    q_idx += 1
+                end   
+            end
         end
     end
+    println("Inventory used (%):")
+    display(inventory_used * 100)
+    println("Staff used (%): ")
+    println(staffing_used * 100)
 end
 
-print("hej")
+checkSolution("output/solution.txt")
