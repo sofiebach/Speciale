e91b9e9beb12eecede0af5a6c3dd785e4e9ab603
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-09-05 16:58:37 +0200

    dr data i model

--- InitialModel.jl
+++ InitialModel.jl
@@ -1,10 +1,12 @@
 using JuMP, Gurobi
 
 include("ReadWrite.jl")
+include("ReadData.jl")
 
 filename = "simulated_data/data.txt"
 
-P,C,timeperiod,L_lower,L_upper,L,L_zero,Q_lower,Q_upper,Q,start,stop,T,S,w,H,I,u = readInstance(filename)
+# P,C,timeperiod,L_lower,L_upper,L,L_zero,Q_lower,Q_upper,Q,start,stop,T,S,w,H,I,u = readInstance(filename)
+P,C,timeperiod,L_lower,L_upper,L,L_zero,Q_lower,Q_upper,Q,start,stop,T,S,w,H,I,u = read_DR_data()
 
 model = Model(Gurobi.Optimizer)
 
@@ -17,10 +19,10 @@ model = Model(Gurobi.Optimizer)
 @constraint(model, [t = (stop + 1):T], sum(x[t,p] for p = 1:P) == 0)
 
 # Inventory from t = start:stop
-@constraint(model, [t=start:stop, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = 1:length(L) for p = 1:P) <= I[t,c])
+@constraint(model, [t=(start+1):(stop-1), c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = 1:length(L) for p = 1:P) <= I[t,c])
 # Inventory for boundaries
-@constraint(model, [t=(stop+1):T, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = (L_zero + 1):length(L) for p = 1:P) <= I[t,c])
-@constraint(model, [t=1:start-1, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = 1:(L_zero - 1) for p = 1:P) <= I[t,c])
+@constraint(model, [t=stop:T, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = (t - stop + L_zero):length(L) for p = 1:P) <= I[t,c])
+@constraint(model, [t=1:start, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = 1:(t - 1 + L_zero) for p = 1:P) <= I[t,c])
 
 # Staff from t = start:stop
 @constraint(model, [t=1:(stop+Q_upper)], sum(w[p] * x[t-Q[q],p] for p=1:P for q = 1:length(Q)) <= H[t])
@@ -46,7 +48,7 @@ function print_solution(model)
             println("Penalty for priority ", p , " with value: ", JuMP.value(f[p]))
         end
     end
-
+    println("Number of campaigns: ", sum(sol))
     return sol
 end
 
