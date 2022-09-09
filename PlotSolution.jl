56af8e1778c30242288ea5efc6f756877cc63b52
thearonn <thea.ronn@hotmail.com>
2022-09-09 14:55:06 +0200

    Plot

--- PlotSolution.jl
+++ PlotSolution.jl
@@ -2,53 +2,79 @@ include("ReadWrite.jl")
 using Luxor
 
 
-x, obj, P, C, timeperiod, L_lower, L_upper, Q_lower, Q_upper, start, stop, T, u = readSolution("output/solution.txt")
+data, sol = readSolution("output/solution.txt")
 
-
-function drawSolution(x, obj, start, stop)
-    scale = 100
-    height = sum(x)+2
-    c = 3
-    w = L_upper
-    Drawing((T+w)*scale, height*scale, "output/schedule.png")
+function drawSolution(data, sol)
+    scalar = 100
+    height = 50#sum(sol.x)+2
+    c = 2
+    w = data.L_upper
+    Drawing((data.T+w)*scalar, height*scalar, "output/schedule.png")
     background("white") # color of background
     origin()
 
     
 
     #transform([1 0 0 -1 0 0])
-    translate(-(T+w)*scale/2, -height*scale/2)
-    colors = vcat(repeat(["blue"],8,), repeat(["red"], 4)) 
-    rect(0,(c-1)*scale,(T+w)*scale,1, :fill)
+    translate(-(data.T+w)*scalar/2, -height*scalar/2)
+    #colors = vcat(repeat(["blue"],7,), repeat(["red"], 5)),repeat(["purple"], 2), repeat(["green"], 3),repeat(["pink"], 4), repeat(["yellow"], 4), repeat(["grey"], 1),repeat(["orange"], 2),repeat(["green"], 1)) 
+    colors = ["black", "blue", "red", "green", "yellow", "orange", "purple", "cyan", "magenta", "lime", "grey", "white"]
+    
+    rect(0,(c-1)*scalar,(data.T+w)*scalar,1, :fill)
   
     
     fontsize(70)
     sethue("black")
     #text("HEJ", Point(100,100), halign=:center, valign = :top)
-    for t = 1:T+w
-        time = t-start+1
-        rect((t-1) * scale,(c-1) * scale ,1,(T+w) * scale, :fill)
+    for t = 1:data.T+w
+        time = t-data.start+1
+        rect((t-1) * scalar,(c-1) * scalar ,1,(height) * scalar, :fill)
         if time > 0
-            text(string(time), Point((t-1)*scale,1 * scale), halign=:center)
+            text(string(time), Point((t-1)*scalar,1 * scalar), halign=:center)
         end
     end
 
-    for t = 1:T
-        for p = 1:P
-            if x[t,p] != 0
-                for n = 1:x[t,p]
-                    sethue(colors[p])
-                    rect((t-1) * scale, (c-1) * scale, w * scale, 0.7  * scale,:fill)
-                    sethue("black")
-                    rect((t-1) * scale, (c-1) * scale, w * scale, 0.7  * scale,:stroke)
-                    c += 1
+    row_start = 3
+    for p = 2:2
+        ends = ones(1)*5
+        for t = 1:data.T
+            if sol.x[t,p] != 0
+                for n = 1:sol.x[t,p]
+                    isPlaced = false
+                    for e in 1:length(ends)
+                        if t >= ends[e]
+                            println("Ends: ", ends[e])
+                            println("T: ", t)
+                            setcolor(colors[p])
+                            setopacity(0.5)
+                            rect((t-1) * scalar, (row_start+e-1) * scalar, w * scalar, 0.7  * scalar,:fill)
+                            sethue("black")
+                            rect((t-1) * scalar, (row_start+e-1) * scalar, w * scalar, 0.7  * scalar,:stroke)
+                            ends[e] = ends[e] + w
+                            isPlaced = true
+                            break
+                        end
+                    end
+                    if isPlaced == false    
+                        sethue(colors[p])
+                        setopacity(0.5)
+                        rect((t-1) * scalar, (c) * scalar, w * scalar, 0.7  * scalar,:fill)
+                        sethue("black")
+                        rect((t-1) * scalar, (c) * scalar, w * scalar, 0.7  * scalar,:stroke)
+                        c += 1
+                        append!(ends,(t-1+w))
+                    end
+                    #print(ends)
                 end
             end
-        end    
+        end
+        c = c+1    
+        row_start = row_start + length(ends)
     end
-    #transform([1 0 0 -1 0 0])
+
     finish()
     preview()
+    #transform([1 0 0 -1 0 0])
 
     #sethue("black")
 #
@@ -81,4 +107,4 @@ function drawSolution(x, obj, start, stop)
     #preview()
 end
 
-drawSolution(x, obj, start, stop)
+drawSolution(data,sol)
