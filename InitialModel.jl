5dc2c15ec39db473e35726dc2b3bb04831783f37
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-09-09 12:06:32 +0200

    object tilføjet til funktioner

--- InitialModel.jl
+++ InitialModel.jl
@@ -1,86 +1,72 @@
 using JuMP, Gurobi
 
 include("ReadWrite.jl")
-include("ReadData.jl")
 
-filename = "simulated_data/data.txt"
+# filename = "simulated_data/data.txt"
+# data = readInstance(filename)
+data = read_DR_data()
 
-# P,C,timeperiod,L_lower,L_upper,L,L_zero,Q_lower,Q_upper,Q,start,stop,T,S,w,H,I,u = readInstance(filename)
-P,C,M,timeperiod,L_lower,L_upper,L,L_zero,Q_lower,Q_upper,Q,start,stop,T,S,w,H,I,u = read_DR_data()
+model = Model(optimizer_with_attributes(Gurobi.Optimizer, "TimeLimit" => 60))
 
-GRP_matrix = zeros(Float64, T, T, P, C)
-for t_col = start:stop
-    row_start = t_col + L_lower
-    row_stop = min(T, row_start + length(L) - 1)
-    for t_row = row_start:row_stop
-        for c = 1:C
-            for p = 1:P
-                l = L_zero + t_row-t_col
-                GRP_matrix[t_row, t_col, p, c] = u[l,p,c]
-            end
-        end
-    end
-end
+@variable(model, x[1:data.T, 1:data.P] >= 0, Int)
+@variable(model, f[1:data.T,1:data.M] >= 0, Int)
+@variable(model, k[1:data.P] >= 0, Int)
 
-model = Model(Gurobi.Optimizer)
+@objective(model, Max, sum(x[t,p] for t = data.start:data.stop for p = 1:data.P) - sum(k[p] for p = 1:data.P) - sum(f[t,m] for t = 1:data.T for m = 1:data.M))
 
-@variable(model, x[1:T, 1:P] >= 0, Int)
-@variable(model, f[1:T,1:M] >= 0, Int)
-@variable(model, k[1:P] >= 0, Int)
-
-@objective(model, Max, sum(x[t,p] for t = start:stop for p = 1:P) - sum(k[p] for p = 1:P) - sum(f[t,m] for t = 1:T for m = 1:M))
-
-@constraint(model, [t = 1:(start - 1)], sum(x[t,p] for p = 1:P) == 0)
-@constraint(model, [t = (stop + 1):T], sum(x[t,p] for p = 1:P) == 0)
+@constraint(model, [t = 1:(data.start - 1)], sum(x[t,p] for p = 1:data.P) == 0)
+@constraint(model, [t = (data.stop + 1):data.T], sum(x[t,p] for p = 1:data.P) == 0)
 
 # Inventory from t = start:stop
-@constraint(model, [t=1:T, c=1:C], sum(GRP_matrix[t,t2,p,c]*x[t2,p] for t2=1:T for p=1:P) <= I[t,c])
+@constraint(model, [t=1:data.T, c=1:data.C], sum(data.GRP_matrix[t,t2,p,c]*x[t2,p] for t2=1:data.T for p=1:data.P) <= data.I[t,c])
 # @constraint(model, [t=(start+1):(stop-1), c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = 1:length(L) for p = 1:P) <= I[t,c])
 # Inventory for boundaries
 # @constraint(model, [t=stop:T, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = (t - stop + L_zero):length(L) for p = 1:P) <= I[t,c])
 # @constraint(model, [t=1:start, c = 1:C], sum(u[l,p,c] * x[t-L[l],p] for l = 1:(t - 1 + L_zero) for p = 1:P) <= I[t,c])
 
 # Staff from t = start:stop
-@constraint(model, [t=1:(stop+Q_upper), m=1:M], sum(w[p,m] * x[t-Q[q],p] for p=1:P for q = 1:length(Q)) <= H[t,m] + 7*3.5*f[t,m])
+@constraint(model, [t=1:(data.stop+data.Q_upper), m=1:data.M], sum(data.w[p,m] * x[t-data.Q[q],p] for p=1:data.P for q = 1:length(data.Q)) <= data.H[t,m] + 7*3.5*f[t,m]) # freelancer * 7 hours * 3,5 days (avg days per week) 
 
 # Scope constraint
-@constraint(model, [p=1:P], sum(x[t,p] for t = start:stop) >= S[p] - k[p])
+@constraint(model, [p=1:data.P], sum(x[t,p] for t = data.start:data.stop) >= data.S[p] - k[p])
 
 JuMP.optimize!(model)
 
-function print_solution(model)
+sol = Sol(data.T,data.P,data.M)
+
+function print_solution(model, sol)
     println("Objective: ", objective_value(model))
-    sol = zeros(Int, T, P)
-    for p = 1:P
-        for t = 1:T
+    sol.obj = objective_value(model)
+    for p = 1:sol.P
+        for t = 1:sol.T
             if JuMP.value(x[t,p]) > 0.5
                 #println("At time ", t, " we have priority ", p, " with value: ", JuMP.value(x[t,p]))
-                sol[t,p] = JuMP.value(x[t,p])
+                sol.x[t,p] = JuMP.value(x[t,p])
             end
         end
-        println("Priority ", p, " is scheduled ", sum(sol[:,p]), " times")
+        println("Priority ", p, " is scheduled ", sum(sol.x[:,p]), " times")
     end
 
-    k1 = zeros(Int, P)
-    for p = 1:P
+    for p = 1:sol.P
         if JuMP.value(k[p]) > 0.5
-            println("Penalty for priority ", p , " with value: ", JuMP.value(k[p]))
-            k1[p] = JuMP.value(k[p])
+            sol.k[p] = JuMP.value(k[p])
+            println("Penalty for priority ", p , " with value: ", sol.k[p])
         end
     end
-    f1 = zeros(Int, T, M)
-    for t = 1:T
-        for m = 1:M
+    for t = 1:sol.T
+        for m = 1:sol.M
             if JuMP.value(f[t,m]) > 0.5
-               println("Number of freelance for media ", m , " at time ", t, ": ", JuMP.value(f[t,m]))
-               f1[t,m] = JuMP.value(f[t,m])
+                sol.f[t,m] = JuMP.value(f[t,m])
+                println("Number of freelance for media ", m , " at time ", t, ": ", sol.f[t,m])
             end
         end
     end
-    println("Number of campaigns: ", sum(sol))
-    return sol, k1, f1
+    println("Number of campaigns: ", sum(sol.x))
+    return sol
 end
 
-sol, k1, f1 = print_solution(model)
+sol = print_solution(model,sol)
 
-writeSolution("output/solution.txt", sol, k1, f1, P, C, M, timeperiod,L_lower,L_upper,L_zero,Q_lower,Q_upper, start, stop, T)
+writeSolution("output/solution.txt", data, sol)
+
+data1, sol1 = readSolution("output/solution.txt")
--- ReadWrite.jl
+++ ReadWrite.jl
@@ -1,111 +1,232 @@
-#Read data instance
-function readInstance(filename)
-    f = open(filename)
-    readline(f) # comment
-    P, C, M, timeperiod = parse.(Int,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    L_lower, L_upper, L_zero = parse.(Int,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    L = parse.(Int,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    Q_lower, Q_upper = parse.(Int,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    Q = parse.(Int,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    start, stop, T = parse.(Int,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    S = parse.(Float64,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    w = parse.(Float64,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    H = zeros(Float64, T, M)
-    for m = 1:M
-        H[:,m] = parse.(Float64,split(readline(f)))
-    end
-    #H = parse.(Int,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    I = zeros(Float64, T, C)
-    for c in 1:C
-        I[:,c] = parse.(Float64,split(readline(f)))
-    end
-    readline(f) # blank
-    readline(f) # comment
-    L = collect(L_lower:L_upper)
-    u = zeros(Float64, length(L), P, C)
-    for c = 1:C
-        line = parse.(Float64,split(readline(f)))
-        u[:,:,c] = reshape(line, (length(L), P))
+using XLSX
+
+# Struct for holding the instance
+mutable struct Instance
+    P::Int64
+    C::Int64
+    M::Int64
+    timeperiod::Int64
+    L_lower::Int64
+    L_upper::Int64
+    L::Array{Int64,1}
+    L_zero::Int64
+    Q_lower::Int64
+    Q_upper::Int64
+    Q::Array{Int64,1}
+    start::Int64
+    stop::Int64
+    T::Int64
+    S::Array{Float64,2}
+    w::Array{Float64,2}
+    H::Array{Float64,2}
+    I::Array{Float64,2}
+    u::Array{Float64, 3}
+    GRP_matrix::Array{Float64, 4}
+    Instance(P,C,M,timeperiod,L_lower,L_upper,Q_lower,Q_upper,T) = new(P,C,M,timeperiod,
+        L_lower,L_upper,collect(L_lower:L_upper),indexin(0,collect(L_lower:L_upper))[],
+        Q_lower,Q_upper,collect(Q_lower:Q_upper),
+        abs(Q_lower)+1,abs(Q_lower)+timeperiod,T,
+        zeros(Float64, T, P),
+        zeros(Float64, P, M),
+        zeros(Float64, T, M),
+        zeros(Float64, T, C),
+        zeros(Float64, length(collect(L_lower:L_upper)), P, C),
+        zeros(Float64, T, T, P, C))
+end
+
+# Struct for holding the instance
+mutable struct Sol
+    obj::Float64
+    x::Array{Int64,2}
+    f::Array{Int64,2}
+    k::Array{Int64,1}
+    P::Int64
+    T::Int64
+    M::Int64
+    Sol(T,P,M) = new(0.0, zeros(Int64,T,P), zeros(Int64,T,M), zeros(Int64,P), P, T, M)
+end
+
+function read_DR_data()
+    P = 29 # skal sættes op, når vi ved noget om Binge, Stacking osv.
+    C = 12
+    M = 4 # medias
+    L_lower = -2
+    L_upper = 5
+    Q_lower = -4
+    Q_upper = -3
+    timeperiod = 52
+    T = abs(Q_lower) + timeperiod + L_upper
+
+    data = Instance(P,C,M,timeperiod,L_lower,L_upper,Q_lower,Q_upper,T)
+    # Read GRP consumption
+    # u[l,p,c] is GRP consumption of priority p on channel c in relative week l-3
+    prefix = "Priority "
+    for p = 1:data.P
+        sheet_name = prefix * string(p)
+        consumption = XLSX.readdata("data/Inventory-consumptiom.xlsx", sheet_name, "C4:J15")
+        consumption = convert(Array{Float64,2}, consumption)
+        data.u[:,p,:] = transpose(consumption)
     end
 
-    return P,C,M,timeperiod,L_lower,L_upper,L,L_zero,Q_lower,Q_upper,Q,start,stop,T,S,w,H,I,u
+    # Read production hours
+    # w[p,m] is production hours of priority p on media m (platforms are TV, RADIO, BANNER, SOME)
+    data.w = convert(Array{Float64,2},XLSX.readdata("data/data_staffing_constraint.xlsx", "Producertimer", "D2:G38"))
+
+    # Read staffing
+    # H[t,m] is weekly staffing (hours) on platform m (medias are TV, RADIO, BANNER, SOME) at time t
+    data.H = transpose(repeat(convert(Array{Float64,2},XLSX.readdata("data/data_staffing_constraint.xlsx", "Bemanding", "E2:E5")),1,data.T))
+
+    # Read scope
+    # S[p] is scope for priority p
+    data.S = convert(Array{Float64,2}, XLSX.readdata("data/data_staffing_constraint.xlsx", "Scope", "D2:D38"))
+
+    # Simulate I for now
+    average = [154 16 6 6 3 50 111 12 2 1 10 10]*7.0 # Banner og SOME er opfundet
+    #1	DR1
+    #2	DR2
+    #3	Ramasjang
+    #4	P1
+    #5	P2
+    #6	P3
+    #7	P4
+    #8	P5
+    #9	P6
+    #10	P8
+    #11	Banner
+    #12	SOME
+    for c = 1:data.C
+        data.I[:, c] = repeat([average[c]], data.T)
+    end
+
+    #GRP_matrix = zeros(Float64, T, T, P, C)
+    for t_col = data.start:data.stop
+        row_start = t_col + data.L_lower
+        row_stop = min(data.T, row_start + length(data.L) - 1)
+        for t_row = row_start:row_stop
+            for c = 1:data.C
+                for p = 1:data.P
+                    l = data.L_zero + t_row-t_col
+                    data.GRP_matrix[t_row, t_col, p, c] = data.u[l,p,c]
+                end
+            end
+        end
+    end
+
+    return data
 end
-#filename = "simulated_data/data.txt"
-#P,C,M,timeperiod,L_lower,L_upper,L,L_zero,Q_lower,Q_upper,Q,start,stop,T,S,w,H,I,u = readInstance(filename)
+
+
+#Read data instance
+#function readInstance(filename)
+#    f = open(filename)
+#    readline(f) # comment
+#    P, C, M, timeperiod = parse.(Int,split(readline(f)))
+#    readline(f) # blank
+#    readline(f) # comment
+#    L_lower, L_upper, L_zero = parse.(Int,split(readline(f)))
+#    readline(f) # blank
+#    readline(f) # comment
+#    L = parse.(Int,split(readline(f)))
+#    readline(f) # blank
+#    readline(f) # comment
+#    Q_lower, Q_upper = parse.(Int,split(readline(f)))
+#    readline(f) # blank
+#    readline(f) # comment
+#    Q = parse.(Int,split(readline(f)))
+#    readline(f) # blank
+#    readline(f) # comment
+#    start, stop, T = parse.(Int,split(readline(f)))
+#    readline(f) # blank
+#    readline(f) # comment
+#    S = parse.(Float64,split(readline(f)))
+#    readline(f) # blank
+#    readline(f) # comment
+#    w = parse.(Float64,split(readline(f)))
+#    readline(f) # blank
+#    readline(f) # comment
+#    H = zeros(Float64, T, M)
+#    for m = 1:M
+#        H[:,m] = parse.(Float64,split(readline(f)))
+#    end
+#    #H = parse.(Int,split(readline(f)))
+#    readline(f) # blank
+#    readline(f) # comment
+#    I = zeros(Float64, T, C)
+#    for c in 1:C
+#        I[:,c] = parse.(Float64,split(readline(f)))
+#    end
+#    readline(f) # blank
+#    readline(f) # comment
+#    L = collect(L_lower:L_upper)
+#    u = zeros(Float64, length(L), P, C)
+#    for c = 1:C
+#        line = parse.(Float64,split(readline(f)))
+#        u[:,:,c] = reshape(line, (length(L), P))
+#    end
+#
+#    GRP_matrix = zeros(Float64, T, T, P, C)
+#    for t_col = start:stop
+#        row_start = t_col + L_lower
+#        row_stop = min(T, row_start + length(L) - 1)
+#        for t_row = row_start:row_stop
+#            for c = 1:C
+#                for p = 1:P
+#                    l = L_zero + t_row-t_col
+#                    GRP_matrix[t_row, t_col, p, c] = u[l,p,c]
+#                end
+#            end
+#        end
+#    end
+#    data = Instance(P,C,M,timeperiod,L_lower,L_upper,L,L_zero,Q_lower,Q_upper,Q,start,stop,T,S,w,H,I,u,GRP_matrix)
+#    return data
+#end
 
 # Write solution
-function writeSolution(filename, sol, k, f, P, C, M, timeperiod, L_lower, L_upper, L_zero, Q_lower,Q_upper, start, stop, T)
+function writeSolution(filename, data, sol)
     outFile = open(filename, "w")
-    write(outFile, "P, C, M, timeperiod" * "\n")
-    write(outFile, join([P, C, M, timeperiod]," ")*"\n\n")
+    write(outFile, "P, C, M, T, timeperiod" * "\n")
+    write(outFile, join([data.P, data.C, data.M, data.T, data.timeperiod]," ")*"\n\n")
 
-    write(outFile, "START, STOP, T" * "\n")
-    write(outFile, join([start, stop, T]," ") * "\n\n")
-
-    write(outFile, "Objective value \n")
-    write(outFile, join(objective_value(model)," ")*"\n\n")
-    write(outFile, "Solution \n")
-    for p = 1:P
-        write(outFile,join(sol[:,p]," ") * "\n")
-    end
-    write(outFile, "\n")
-
-    write(outFile, "Penalty for undone jobs" * "\n")
-    write(outFile,join(k," ")*"\n")
-    write(outFile, "\n")
-
-    write(outFile, "Freelancers hired" * "\n")
-    for m = 1:M
-        write(outFile,join(f[:,m]," ")*"\n")
-    end
-    write(outFile, "\n")
-
-    write(outFile, "L_lower, L_upper, L0" * "\n")
-    write(outFile, join([L_lower, L_upper, L_zero]," ")*"\n\n")
+    write(outFile, "L_lower, L_upper" * "\n")
+    write(outFile, join([data.L_lower, data.L_upper]," ")*"\n\n")
 
     write(outFile, "Q_lower, Q_upper" * "\n")
-    write(outFile, join([Q_lower, Q_upper]," ")*"\n\n")
-
-    # Time periods incl. prod and air
-    start = abs(Q_lower) + 1
-    stop = abs(Q_lower) + timeperiod
+    write(outFile, join([data.Q_lower, data.Q_upper]," ")*"\n\n")
 
     write(outFile, "CONSUMPTION" * "\n")
-    for c = 1:C
-        write(outFile, join(u[:,:,c], " ") * "\n")
+    for c = 1:data.C
+        write(outFile, join(data.u[:,:,c], " ") * "\n")
     end
     write(outFile, "\n")
 
     write(outFile, "INVENTORY" * "\n")
-    for c = 1:C
-        write(outFile, join(I[:,c], " ") * "\n")
+    for c = 1:data.C
+        write(outFile, join(data.I[:,c], " ") * "\n")
     end
     write(outFile, "\n")
 
     write(outFile, "STAFFING" * "\n")
-    for m = 1:M
-        write(outFile, join(H[:,m], " ") * "\n")
+    for m = 1:data.M
+        write(outFile, join(data.H[:,m], " ") * "\n")
     end
     write(outFile, "\n")
 
+    write(outFile, "Objective value \n")
+    write(outFile, join(sol.obj," ")*"\n\n")
+    write(outFile, "Solution \n")
+    for p = 1:sol.P
+        write(outFile,join(sol.x[:,p]," ") * "\n")
+    end
+    write(outFile, "\n")
+
+    write(outFile, "Penalty for undone jobs" * "\n")
+    write(outFile,join(sol.k," ")*"\n")
+    write(outFile, "\n")
+
+    write(outFile, "Freelancers hired" * "\n")
+    for m = 1:sol.M
+        write(outFile,join(sol.f[:,m]," ")*"\n")
+    end
     close(outFile)
 end
 
@@ -114,55 +235,49 @@ end
 function readSolution(filename)
     f = open(filename)
     readline(f) # comment
-    P, C, M, timeperiod = parse.(Int,split(readline(f)))
+    P, C, M, T, timeperiod = parse.(Int,split(readline(f)))
     readline(f) # blank
     readline(f) # comment
-    start, stop, T = parse.(Int,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    obj = parse.(Float64, readline(f))
-    readline(f) # blank
-    readline(f) # comment
-    x = zeros(Int, T, P)
-    for p in 1:P
-        x[:,p] = parse.(Int,split(readline(f)))
-    end
-    readline(f) # blank
-    readline(f) # comment
-    k = parse.(Int,split(readline(f)))
-    readline(f) # blank
-    readline(f) # comment
-    f1 = zeros(Int, T, M)
-    for m = 1:M
-        f1[:,m] = parse.(Int,split(readline(f)))
-    end
-    readline(f) # blank
-    readline(f) # comment
-    L_lower, L_upper, L_zero = parse.(Int,split(readline(f)))
+    L_lower, L_upper = parse.(Int,split(readline(f)))
     readline(f) # blank
     readline(f) # comment
     Q_lower, Q_upper = parse.(Int,split(readline(f)))
     readline(f) # blank
     readline(f) # comment
-    L = collect(L_lower:L_upper)
-    
-    u = zeros(Float64, length(L), P, C)
+    data = Instance(P,C,M,timeperiod,L_lower,L_upper,Q_lower,Q_upper,T)
     for c = 1:C
         line = parse.(Float64,split(readline(f)))
-        u[:,:,c] = reshape(line, (length(L), P))
+        data.u[:,:,c] = reshape(line, (length(data.L), data.P))
     end
     readline(f) # blank
     readline(f) # comment
-    I = zeros(Float64, T, C)
-    for c in 1:C
-        I[:,c] = parse.(Float64,split(readline(f)))
+    for c in 1:data.C
+        data.I[:,c] = parse.(Float64,split(readline(f)))
     end
     readline(f) # blank
     readline(f) # comment
-    H = zeros(Float64, T, M)
+    for m = 1:data.M
+        data.H[:,m] = parse.(Float64,split(readline(f)))
+    end
+    readline(f) # blank
+    readline(f) # comment
+    sol = Sol(data.T,data.P,data.M)
+    sol.obj = parse.(Float64, readline(f))
+    readline(f) # blank
+    readline(f) # comment
+    #x = zeros(Int, T, P)
+    for p in 1:data.P
+        sol.x[:,p] = parse.(Int,split(readline(f)))
+    end
+    readline(f) # blank
+    readline(f) # comment
+    sol.k = parse.(Int,split(readline(f)))
+    readline(f) # blank
+    readline(f) # comment
     for m = 1:M
-        H[:,m] = parse.(Float64,split(readline(f)))
+        sol.f[:,m] = parse.(Int,split(readline(f)))
     end
-    return x, obj, f1, k, P, C, M, timeperiod, L_lower, L_upper, Q_lower, Q_upper, start, stop, T, u, I, H
-
+    readline(f) # blank
+    readline(f) # comment
+    return data, sol
 end
