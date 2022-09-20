1ac5d80a2397ad76229c38a27c23365d9b200bbd
thearonn <thea.ronn@hotmail.com>
2022-09-20 14:38:54 +0200

    :)

--- main.jl
+++ main.jl
@@ -2,35 +2,37 @@ include("ReadWrite.jl")
 include("MIPModel.jl")
 include("ValidateSolution.jl")
 include("PlotSolution.jl")
-include("DR_output.jl")
+#include("DR_output.jl")
 
 P = 29
 data = read_DR_data(P)
 
 # Replace with DR inventory and DR scope
-I, S = DR_plan()
-data.I[data.start:data.stop,:] = I
-data.S = S[1:data.P]
+#I, S = DR_plan()
+#data.I[data.start:data.stop,:] = I
+#data.S = S[1:data.P]
 
-sol = MIP(data, 30)
+sol = MIP(data, 60)
 
-println("DR plans: ", sum(S[1:data.P]))
-println("MIP plans: ", sum(sol.x))
+#println("DR plans: ", sum(S[1:data.P]))
+#println("MIP plans: ", sum(sol.x))
 
 print_solution(sol)
 
 checkSolution(data,sol)
 
-#writeSolution(filename, data, sol)
 
-#filename = "output/solution.txt"
-#data, sol = readSolution(filename)
+
+filename = "output/solution.txt"
+#writeSolution(filename, data, sol)
+data, sol = readSolution(filename)
 
 
 
 drawSolution(data,sol)
 
+include("PlotSolution.jl")
 drawHeatmap(data,sol)
 
-plotScope(data, sol)
+#plotScope(data, sol)
 
