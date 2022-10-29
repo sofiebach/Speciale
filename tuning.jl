ccd08cf4e6d79b5a0a57df17702cbc1de7561117
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-10-29 10:31:32 +0200

    tuning script

---
+++ tuning.jl
@@ -0,0 +1,143 @@
+include("ALNS.jl")
+include("ReadWrite.jl")
+include("ConstructionHeuristics.jl")
+include("MIPModel.jl")
+
+P = 37
+data = read_DR_data(P)
+
+function tuneAcceptanceCriteria(data, temperatures, alphas)
+    filename = "tuning/T_alpha"
+    outFile = open(filename, "w")
+    time_limit = 10
+    num_iter = 5
+
+    best_obj = Inf
+    best_T = 0
+    best_alpha = 0
+    T_idx = 1
+    for T in temperatures
+        a_idx = 1
+        for alpha in alphas
+            println("T: ", T, " alpha: ", alpha)
+            objs = 0
+            for k = 1:num_iter
+                sol, _ = ALNS(data, time_limit, T, alpha)
+                objs += sol.obj
+            end
+            write(outFile, "T, alpha, avg objective\n")
+            write(outFile, join([T, alpha, objs/num_iter]," ")*"\n")
+            if objs / num_iter < best_obj 
+                best_obj = objs / num_iter
+                best_T = T
+                best_alpha = alpha
+            end
+            a_idx += 1
+        end
+        T_idx += 1
+    end
+    close(outFile)
+    return best_T, best_alpha
+end
+
+function tuneDestroy(data, cluster, random, worst, related)
+    time_limit = 10
+    num_iter = 5
+    T = 1000
+    alpha = 0.99
+    
+    best_obj = Inf
+    best_cluster = 0
+    best_random = 0
+    best_worst = 0
+    best_related = 0
+
+    filename = "tuning/destroy_fracs"
+    outFile = open(filename, "w")
+    for frac_cluster in cluster, frac_random in random, thres_worst in worst, frac_related in related
+        println("cluster: ", frac_cluster)
+        println("random: ", frac_random)
+        println("worst: ", thres_worst)
+        println("related: ", frac_related)
+        objs = 0
+        for k = 1:num_iter
+            sol, _ = ALNS(data, time_limit, T, alpha, frac_cluster, frac_random, thres_worst, frac_related)
+            objs += sol.obj
+        end
+        write(outFile, "cluster, random, worst, related, avg objective\n")
+        write(outFile, join([frac_cluster, frac_random, thres_worst, frac_related, objs/num_iter]," ")*"\n")
+        if objs / num_iter < best_obj 
+            best_obj = objs / num_iter
+            best_cluster = frac_cluster
+            best_random = frac_random
+            best_worst = thres_worst
+            best_related = frac_related
+        end
+    end
+    close(outFile)
+    return best_cluster, best_random, best_worst, best_related
+end
+
+function readTuneDestroy()
+    filename = "tuning/destroy_fracs"
+    file = open(filename)
+
+    params = []
+    objectives = []
+
+    comment = 1
+    for line in eachline(file)
+        if comment % 2 == 0
+            frac_cluster, frac_random, thres_worst, frac_related, obj = parse.(Float64, split(line))
+            push!(params, [frac_cluster, frac_random, thres_worst, frac_related])
+            push!(objectives, obj)
+        end
+        comment += 1
+    end
+    close(file)
+
+    return params, objectives
+end
+
+function readTuneAcceptance()
+    filename = "tuning/T_alpha"
+    file = open(filename)
+
+    params = []
+    objectives = []
+
+    comment = 1
+    for line in eachline(file)
+        if comment % 2 == 0
+            T, alpha, obj = parse.(Float64, split(line))
+            push!(params, [T, alpha])
+            push!(objectives, obj)
+        end
+        comment += 1
+    end
+    close(file)
+
+    return params, objectives
+end
+
+
+# temperatures = collect(500:100:1000)
+# alphas = collect(0.70:0.02:0.99)
+temperatures = [100, 1000]
+alphas = [0.8, 0.9]
+
+T, alpha = tuneAcceptanceCriteria(data, temperatures, alphas)
+
+T_alpha, objectives1 = readTuneAcceptance()
+
+# cluster = collect(0.1:0.05:0.3)
+# random = collect(0.1:0.05:0.3)
+# worst = collect(5:1:10)
+# related = collect(0.1:0.05:0.3)
+cluster = [0.1, 0.2]
+random = [0.1, 0.2]
+worst = [5, 10]
+related = [0.1, 0.2]
+frac_cluster, frac_random, thres_worst, frac_related = tuneDestroy(data, cluster, random, worst, related)
+
+fracs, objectives2 = readTuneDestroy()
