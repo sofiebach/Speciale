d043ff12eb9df5ec03c1fe8af7db1de537abdb8f
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-11-01 11:24:01 +0100

    profiling + alns uden modelrepair

--- Expansion/ALNS_expanded.jl
+++ Expansion/ALNS_expanded.jl
@@ -346,7 +346,7 @@ function selectMethod(prob)
     end
 end
 
-function ALNS(data, time_limit, T=1000, alpha=0.999, frac_cluster=0.1, frac_random=0.1, thres_worst=10, frac_related=0.2)
+function ALNS(data, time_limit, T=1000, alpha=0.999, frac_cluster=0.1, frac_random=0.1, thres_worst=10, frac_related=0.2)    
     it = 0
     sol = randomInitial(data)
     best_sol = deepcopy(sol)
@@ -354,10 +354,14 @@ function ALNS(data, time_limit, T=1000, alpha=0.999, frac_cluster=0.1, frac_rand
     start_time = time_ns()
 
     rho_destroy = ones(4)
-    rho_repair = ones(4)
-
+    time_destroy = zeros(4)
     prob_destroy = zeros(4)
+    num_destroy = zeros(4)
+
+    rho_repair = ones(4)
     prob_repair = zeros(4)
+    time_repair = zeros(4)
+    num_repair = zeros(4)
 
     prob_destroy = setProb(rho_destroy, prob_destroy)
     prob_repair = setProb(rho_repair, prob_repair)
@@ -388,20 +392,30 @@ function ALNS(data, time_limit, T=1000, alpha=0.999, frac_cluster=0.1, frac_rand
         # Choose destroy method
         selected_destroy = selectMethod(prob_destroy)
         if selected_destroy == 1
+            destroy_time = time_ns()
             clusterDestroy!(data,temp_sol,frac_cluster)
         elseif selected_destroy == 2
+            destroy_time = time_ns()
             randomDestroy!(data,temp_sol,frac_random)
         elseif selected_destroy == 3
+            destroy_time = time_ns()
             worstDestroy!(data,temp_sol,thres_worst)
         else
+            destroy_time = time_ns()
             relatedDestroy!(data, sol, frac_related)
         end
-        
+
+        # Update destroy time
+        time_destroy[selected_destroy] += elapsed_time(destroy_time)
+        num_destroy[selected_destroy] += 1
+
         # Choose repair method
         selected_repair = selectMethod(prob_repair)
         
         if selected_repair == 1
+            repair_time = time_ns()
             greedyRepair!(data,temp_sol)
+            elapsed_repair = elapsed_time(repair_time)
             # Check if P_bar constraint is exceeded
             for p_bar in data.P_bar 
                 if temp_sol.k[p_bar] > 0
@@ -411,8 +425,9 @@ function ALNS(data, time_limit, T=1000, alpha=0.999, frac_cluster=0.1, frac_rand
                 end
             end
         elseif selected_repair == 2
+            repair_time = time_ns()
             firstRepair!(data,temp_sol)
-
+            elapsed_repair = elapsed_time(repair_time)
             # Check if P_bar constraint is exceeded
             for p_bar in data.P_bar 
                 if temp_sol.k[p_bar] > 0
@@ -422,8 +437,9 @@ function ALNS(data, time_limit, T=1000, alpha=0.999, frac_cluster=0.1, frac_rand
                 end
             end
         elseif selected_repair == 3 
+            repair_time = time_ns()
             regretRepair!(data,temp_sol)
-
+            elapsed_repair = elapsed_time(repair_time)
             # Check if P_bar constraint is exceeded
             for p_bar in data.P_bar 
                 if temp_sol.k[p_bar] > 0
@@ -433,12 +449,19 @@ function ALNS(data, time_limit, T=1000, alpha=0.999, frac_cluster=0.1, frac_rand
                 end
             end
         else
+            repair_time = time_ns()
             modelRepair!(data,temp_sol)
+            elapsed_repair = elapsed_time(repair_time)
         end
 
         if !valid
             continue
         end
+
+        # Update repair time
+        time_repair[selected_repair] += elapsed_repair
+        num_repair[selected_repair] += 1
+
         append!(repairs, selected_repair)
         append!(destroys, selected_destroy)
         append!(current_obj, temp_sol.obj)
@@ -471,5 +494,156 @@ function ALNS(data, time_limit, T=1000, alpha=0.999, frac_cluster=0.1, frac_rand
         T = alpha * T
      
     end
-    return best_sol, (prob_destroy=prob_destroy, prob_repair=prob_repair, destroys=destroys, repairs=repairs, current_obj=current_obj, current_best=current_best, status=status)
+    return best_sol, (prob_destroy=prob_destroy, prob_repair=prob_repair, destroys=destroys, repairs=repairs, current_obj=current_obj,
+    current_best=current_best, status=status, time_repair=time_repair, time_destroy=time_destroy, num_repair=num_repair, num_destroy=num_destroy)
+end
+
+
+
+function ALNS_uden_modelrepair(data, time_limit, T=1000, alpha=0.999, frac_cluster=0.1, frac_random=0.1, thres_worst=10, frac_related=0.2)    
+    it = 0
+    sol = randomInitial(data)
+    best_sol = deepcopy(sol)
+    temp_sol = deepcopy(sol)
+    start_time = time_ns()
+
+    rho_destroy = ones(4)
+    time_destroy = zeros(4)
+    prob_destroy = zeros(4)
+    num_destroy = zeros(4)
+
+    rho_repair = ones(3)
+    prob_repair = zeros(3)
+    time_repair = zeros(3)
+    num_repair = zeros(3)
+
+    prob_destroy = setProb(rho_destroy, prob_destroy)
+    prob_repair = setProb(rho_repair, prob_repair)
+
+    w1 = 10
+    w2 = 5
+    w3 = 1
+    w4 = 0
+
+    gamma = 0.9
+
+    destroys = []
+    repairs = []
+    current_obj = []
+    current_best = []
+    status = []
+
+    while elapsed_time(start_time) < time_limit
+    #while it < 100
+        valid = true
+        it += 1
+        # update probabilities
+        if (it % 10 == 0)
+            prob_destroy = setProb(rho_destroy, prob_destroy)
+            prob_repair = setProb(rho_repair, prob_repair)
+        end
+
+        # Choose destroy method
+        selected_destroy = selectMethod(prob_destroy)
+        if selected_destroy == 1
+            destroy_time = time_ns()
+            clusterDestroy!(data,temp_sol,frac_cluster)
+        elseif selected_destroy == 2
+            destroy_time = time_ns()
+            randomDestroy!(data,temp_sol,frac_random)
+        elseif selected_destroy == 3
+            destroy_time = time_ns()
+            worstDestroy!(data,temp_sol,thres_worst)
+        else
+            destroy_time = time_ns()
+            relatedDestroy!(data, sol, frac_related)
+        end
+
+        # Update destroy time
+        time_destroy[selected_destroy] += elapsed_time(destroy_time)
+        num_destroy[selected_destroy] += 1
+
+        # Choose repair method
+        selected_repair = selectMethod(prob_repair)
+        
+        if selected_repair == 1
+            repair_time = time_ns()
+            greedyRepair!(data,temp_sol)
+            elapsed_repair = elapsed_time(repair_time)
+            # Check if P_bar constraint is exceeded
+            for p_bar in data.P_bar 
+                if temp_sol.k[p_bar] > 0
+                    temp_sol = deepcopy(sol)
+                    valid = false
+                    break
+                end
+            end
+        elseif selected_repair == 2
+            repair_time = time_ns()
+            firstRepair!(data,temp_sol)
+            elapsed_repair = elapsed_time(repair_time)
+            # Check if P_bar constraint is exceeded
+            for p_bar in data.P_bar 
+                if temp_sol.k[p_bar] > 0
+                    temp_sol = deepcopy(sol)
+                    valid = false
+                    break
+                end
+            end
+        else
+            repair_time = time_ns()
+            regretRepair!(data,temp_sol)
+            elapsed_repair = elapsed_time(repair_time)
+            # Check if P_bar constraint is exceeded
+            for p_bar in data.P_bar 
+                if temp_sol.k[p_bar] > 0
+                    temp_sol = deepcopy(sol)
+                    valid = false
+                    break
+                end
+            end
+        end
+
+        if !valid
+            continue
+        end
+
+        # Update repair time
+        time_repair[selected_repair] += elapsed_repair
+        num_repair[selected_repair] += 1
+
+        append!(repairs, selected_repair)
+        append!(destroys, selected_destroy)
+        append!(current_obj, temp_sol.obj)
+        w = w4
+
+        # Check acceptance criteria
+        if temp_sol.obj < sol.obj
+            sol = deepcopy(temp_sol)
+            w = w2
+            #println("Better than sol")
+        else
+            if rand() < exp(-(temp_sol.obj-sol.obj)/T)
+                sol = deepcopy(temp_sol)
+                w = w3
+                #println("Annealing")
+            end
+        end
+
+        if temp_sol.obj < best_sol.obj
+            best_sol = deepcopy(temp_sol)
+            w = w1
+            println("New best")
+            println(best_sol.obj)
+        end
+        append!(status, w)
+        append!(current_best, best_sol.obj)
+
+        rho_destroy[selected_destroy] = gamma*rho_destroy[selected_destroy] + (1-gamma)*w
+        rho_repair[selected_repair] = gamma*rho_repair[selected_repair] + (1-gamma)*w
+        T = alpha * T
+     
+    end
+    return best_sol, (prob_destroy=prob_destroy, prob_repair=prob_repair, destroys=destroys, repairs=repairs, current_obj=current_obj, 
+    current_best=current_best, status=status, time_repair=time_repair, time_destroy=time_destroy, num_repair=num_repair, num_destroy=num_destroy)
 end
--- Expansion/profiling_ALNS.jl
+++ Expansion/profiling_ALNS.jl
@@ -2,7 +2,6 @@ include("ALNS_expanded.jl")
 include("../tuning/tuning.jl")
 include("../ReadWrite.jl")
 
-
 # Read best T and best alpha
 params1, objs1 = readTuneAcceptance()
 _, idx1 = findmin(objs1)
@@ -25,10 +24,27 @@ P = 37
 data = read_DR_data(P)
 
 # Run ALNS with tuned parameters
-time_limit = 60
-sol, params = ALNS(data, time_limit, T, alpha, frac_cluster, frac_random, thres_worst, frac_related)
+time_limit = 120
+sol1, params1 = ALNS(data, time_limit, T, alpha, frac_cluster, frac_random, thres_worst, frac_related)
+sol2, params2 = ALNS_uden_modelrepair(data, time_limit, T, alpha, frac_cluster, frac_random, thres_worst, frac_related)
 
+println("--- ALNS with model repair ---")
+num_iter1 = length(params1.current_obj)
+println("Iterations: ", num_iter1)
+println("Repair times: ", params1.time_repair)
+avg_time_destroy1 = params1.time_destroy ./ params1.num_destroy
+println("Average destroy times: ", avg_time_destroy1)
+avg_time_repair1 = params1.time_repair ./ params1.num_repair
+println("Average repair times: ", avg_time_repair1)
 
+println("--- ALNS without model repair ---")
+num_iter2 = length(params2.current_obj)
+println("Iterations: ", num_iter2)
+println("Repair times: ", params2.time_repair)
+avg_time_destroy2 = params2.time_destroy ./ params2.num_destroy
+println("Average destroy times: ", avg_time_destroy2)
+avg_time_repair2 = params2.time_repair ./ params2.num_repair
+println("Average repair times: ", avg_time_repair2)
 
 
 
