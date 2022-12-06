c4242fca31699fd0e34c35e14f3a77a987b60a73
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-12-06 13:11:21 +0100

    he

--- sof_test.jl
+++ sof_test.jl
@@ -4,11 +4,11 @@ include("ConstructionHeuristics.jl")
 include("ALNS.jl")
 include("Validation/PlotSolution.jl")
 
-data = readInstance("dataset/25_0_0.txt")
+data = readInstance("dataset/100_0_0.txt")
 
 sol = randomInitial(data)
-time_limit = 60
-sol, params = ALNS(data,sol,time_limit,"expanded")
+time_limit = 60*5
+sol, params = ALNS(data,sol,time_limit,"expanded",true)
 probabilityTracking(params, "prob")
 solutionTracking_all(params, "sol")
 temperatureTracking(params, "temp")
