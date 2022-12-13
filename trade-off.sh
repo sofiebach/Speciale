309631f9f88f014679a449139a656456b8cd67bd
thearonn <thea.ronn@hotmail.com>
2022-12-13 14:12:51 +0100

    Push it

--- trade-off.sh
+++ trade-off.sh
@@ -29,4 +29,4 @@ module load gurobi/9.5.2
 module load julia/1.7.0
 
 # here follow the commands you want to execute
-julia-current generate_tradeoff.jl &> results_test.txt
+julia-current TradeOff/generate_tradeoff.jl &> results_test.txt
