02cc92c0b58a61cd70cd59deabf4fe02d58926d5
thearonn <thea.ronn@hotmail.com>
2022-12-15 10:29:10 +0100

    hej

--- HPC_test.jl
+++ HPC_test.jl
@@ -2,9 +2,10 @@ include("ReadWrite.jl")
 
 idx = parse(Int64, ENV["LSB_JOBINDEX"])
 
-filename = joinpath.("dataset/train/", readdir("dataset/train/"))[idx]
-outFile = open("test/" * string(filename[15:21]), "w")
-data = readInstance(filename)
+filepath = joinpath.("dataset/train/", readdir("dataset/train/"))[idx]
+filename = split(split(path, ".")[1],"/")[3]
+outFile = open("test/" * filename, "w")
+data = readInstance(filepath)
 write(outFile, "Scope\n")
 write(outFile, join(data.S," ")*"\n\n")
 close(outFile)
