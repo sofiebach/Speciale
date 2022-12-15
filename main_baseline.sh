c89adce0637fde7b166958c629173ba9f858e7e8
thearonn <thea.ronn@hotmail.com>
2022-12-15 10:52:54 +0100

    fdf

--- main_baseline.sh
+++ main_baseline.sh
@@ -31,4 +31,4 @@ module load gurobi/9.5.2
 module load julia/1.7.0
 
 # here follow the commands you want to execute
-julia-current main_baseline_test.jl &> results_test.txt
+julia-current main_baseline_test.jl &> results_test_baseline.txt
