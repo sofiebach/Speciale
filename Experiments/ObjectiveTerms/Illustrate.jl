57dfc68d024910085a0c082bb3efb1823b2a34a9
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-12-18 17:59:09 +0100

    aolution plots

--- Experiments/ObjectiveTerms/Illustrate.jl
+++ Experiments/ObjectiveTerms/Illustrate.jl
@@ -1,12 +1,12 @@
 include("../../ReadWrite.jl")
 include("../../Validation/PlotSolution.jl")
 
-filepath = joinpath.("Experiments/ObjectiveTerms/results", readdir("Experiments/ObjectiveTerms/results"))
+filepath = joinpath.("Experiments/ObjectiveTerms/results", readdir("Experiments/ObjectiveTerms/results"))[5:8]
 
 for file in filepath
     prefix = "Experiments/ObjectiveTerms/"
     filename = split(split(file, ".")[1],"/")[4]
-    data = readInstance("dataset/train/"*filename[1:8]*".txt")
+    data = readInstance("dataset/train/"*filename[1:6]*".txt")
     sol = readSolution(file, data)
     drawTVSchedule(data, sol, prefix*filename)
 end
