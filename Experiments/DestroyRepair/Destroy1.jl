fafbbabe05b246e354a7d0b48a084fadc8a896a0
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-12-22 16:10:35 +0100

    destroy repair

--- Experiments/DestroyRepair/Destroy1.jl
+++ Experiments/DestroyRepair/Destroy1.jl
@@ -10,19 +10,36 @@ filename = split(split(filepath, ".")[1],"/")[3]
 data = readInstance(filepath)
 folder = "Experiments/DestroyRepair/initials/"
 init_sol = readSolution(folder*filename, data)
+init_obj = init_sol.exp_obj
 
 destroy_functions = [horizontalDestroy!,verticalDestroy!,randomDestroy!,relatedDestroy!,worstIdleDestroy!,stackDestroy!]
 repair_functions = [greedyRepair!, firstRepair!,flexibilityRepair!,bestRepair!,modelRepair!,horizontalModelRepair!,regretRepair!]
 
-N = 1
-# time_limit = data.timeperiod * 60
+N = 3
+M = length(destroy_functions)
+time_limit = data.timeperiod * 60
 time_limit = 10
 type = "extended"
-for i = 1:length(destroy_functions)
+imps = zeros(Float64, M)
+stds = zeros(Float64, M)
+for i = 1:M
+    objs = zeros(Float64, N)
     destroys = deepcopy(destroy_functions)
     deleteat!(destroys, i)
     for n = 1:N
-        sol, params = ALNS_analysis(data,init_sol,time_limit,type,destroys,repair_functions)    
+        sol, params = ALNS_analysis(data,init_sol,time_limit,type,destroys,repair_functions)
+        objs[n] = sol.exp_obj
     end
+    imps[i] = 100 * (init_obj - mean(objs)) / init_obj
+    stds[i] = std(objs)
 end
+
+outFile = open("Experiments/DestroyRepair/results/destroy/" * filename, "w")
+write(outFile, "functions\n")
+write(outFile, join(destroy_functions," ")*"\n\n")
+write(outFile, "average imps\n")
+write(outFile, join(imps," ")*"\n\n")
+write(outFile, "standard deviations\n")
+write(outFile, join(stds," ")*"\n\n")
+close(outFile)
  
