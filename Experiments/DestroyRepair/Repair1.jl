f2c56b24efc35c973f2146f109f93d61b6d1deaa
sofiebach <70587917+sofiebach@users.noreply.github.com>
2023-01-03 11:06:35 +0100

    slet en masse files

--- Experiments/DestroyRepair/Repair1.jl
+++ Experiments/DestroyRepair/Repair1.jl
@@ -1,8 +1,7 @@
 include("../../ReadWrite.jl")
-include("ALNS_analysis.jl")
+include("../../ALNS.jl")
 
 idx = parse(Int64, ENV["LSB_JOBINDEX"])
-# idx = 4
 
 filepath = joinpath.("dataset/train/", readdir("dataset/train/"))[idx]
 filename = split(split(filepath, ".")[1],"/")[3]
@@ -12,22 +11,21 @@ folder = "Experiments/DestroyRepair/initials/"
 init_sol = readSolution(folder*filename, data)
 init_obj = init_sol.exp_obj
 
-destroy_functions = [horizontalDestroy!,verticalDestroy!,randomDestroy!,relatedDestroy!,worstIdleDestroy!,stackDestroy!]
-repair_functions = [greedyRepair!, firstRepair!,flexibilityRepair!,bestRepair!,modelRepair!,horizontalModelRepair!,regretRepair!]
+destroys = true*ones(Bool, 6)
+repairs = true*ones(Bool, 7)
 
-N = 3
-M = length(repair_functions)
+N = 5
+M = length(repairs)
 time_limit = data.timeperiod * 60
-# time_limit = 10
 type = "extended"
 imps = zeros(Float64, M)
 stds = zeros(Float64, M)
 for i = 1:M
     objs = zeros(Float64, N)
-    repairs = deepcopy(repair_functions)
-    deleteat!(repairs, i)
+    temp_repairs = deepcopy(repairs)
+    temp_repairs[i] = false
     for n = 1:N
-        sol, params = ALNS_analysis(data,init_sol,time_limit,type,destroy_functions,repairs)
+        sol, params = ALNS_final(data,init_sol,time_limit,type,temp_repairs,destroys)
         objs[n] = sol.exp_obj
     end
     imps[i] = 100 * (init_obj - mean(objs)) / init_obj
@@ -35,8 +33,6 @@ for i = 1:M
 end
 
 outFile = open("Experiments/DestroyRepair/results/repair/" * filename, "w")
-write(outFile, "functions\n")
-write(outFile, join(repair_functions," ")*"\n\n")
 write(outFile, "average imps\n")
 write(outFile, join(imps," ")*"\n\n")
 write(outFile, "standard deviations\n")
