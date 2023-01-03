5e76a7278b4c7098bf1eddb84e5777aeadfc4d2b
thearonn <thea.ronn@hotmail.com>
2023-01-03 10:41:34 +0100

    mainfiles til MIP experiments

--- Experiments/MIP/main_baseline_1thread.jl
+++ Experiments/MIP/main_baseline_1thread.jl
@@ -7,10 +7,11 @@ idx = parse(Int64, ENV["LSB_JOBINDEX"])
 filepath = joinpath.("dataset/test/", readdir("dataset/test/"))[idx]
 filename = split(split(filepath, ".")[1],"/")[3]
 folder = "Experiments/MIP/results/"
+timelimit = data.timeperiod*60
 
 data = readInstance(filepath)
 
-x, gap, time = MIPExtended(data, "Gurobi", 0, data.timeperiod*60)
+x, gap, time = MIPBaseline(data, "Gurobi", 1, timelimit, 0, 1)
 sol = MIPtoSol(data,x)
 
-writeSolution(folder * filename * "_extended", data, sol, gap, time)
+writeSolution(folder * filename * "_baseline1thread", data, sol, gap, time)
