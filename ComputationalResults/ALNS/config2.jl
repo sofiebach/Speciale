d45be70dac4644c294a735e9c107b760eea52537
thearonn <thea.ronn@hotmail.com>
2023-01-06 21:03:42 +0100

    hej

--- ComputationalResults/ALNS/config2.jl
+++ ComputationalResults/ALNS/config2.jl
@@ -1,32 +1,36 @@
 include("../../ReadWrite.jl")
 include("../../ALNS.jl")
 
-idx = parse(Int64, ENV["LSB_JOBINDEX"])
-#idx = 4
+function dummyfunction()
+    idx = parse(Int64, ENV["LSB_JOBINDEX"])
+    #idx = 4
 
-configuration = "config2/"
+    configuration = "config2/"
 
-filepath = joinpath.("dataset/test/", readdir("dataset/test/"))[idx]
-filename = split(split(filepath, ".")[1],"/")[3]
-folder = "ComputationalResults/ALNS/results/"
-data = readInstance(filepath)
+    filepath = joinpath.("dataset/test/", readdir("dataset/test/"))[idx]
+    filename = split(split(filepath, ".")[1],"/")[3]
+    folder = "ComputationalResults/ALNS/results/"
+    data = readInstance(filepath)
 
-timelimit = data.timeperiod*60
-#timelimit = 5
-best_i = 0
-best_obj = 1000
+    timelimit = data.timeperiod*60
+    #timelimit = 5
+    best_i = 0
+    best_obj = 1000
 
-N = 20
-#N = 3
-for i = 1:N
-    sol = randomInitial(data)
-    sol, params = ALNS_final(data, sol, timelimit, "extended", [false,false,false,true,true,false,false],[true,false,true,false,false,false])
-    if sol.exp_obj < best_obj
-        best_obj = sol.exp_obj
-        best_i = i
+    N = 20
+    #N = 3
+    for i = 1:N
+        sol = randomInitial(data)
+        sol, params = ALNS_final(data, sol, timelimit, "extended", [false,false,false,true,true,false,false],[true,false,true,false,false,false])
+        if sol.exp_obj < best_obj
+            best_obj = sol.exp_obj
+            best_i = i
+        end
+        writeSolution(folder * configuration * filename * "_solution_" * string(i), data, sol)
+        writeParameters(folder * configuration * filename * "_params_" * string(i), params)
     end
-    writeSolution(folder * configuration * filename * "_solution_" * string(i), data, sol)
-    writeParameters(folder * configuration * filename * "_params_" * string(i), params)
+
+    println("---- best i is " * string(best_i)* " ----")
 end
 
-println("---- best i is " * string(best_i)* " ----")
+dummyfunction()
