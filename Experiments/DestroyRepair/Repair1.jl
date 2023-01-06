33a7efe90807ee7f7502846af9c26ea0e5af2596
sofiebach <70587917+sofiebach@users.noreply.github.com>
2023-01-06 08:04:26 +0100

    repair

--- Experiments/DestroyRepair/Repair1.jl
+++ Experiments/DestroyRepair/Repair1.jl
@@ -2,7 +2,6 @@ include("../../ReadWrite.jl")
 include("../../ALNS.jl")
 
 idx = parse(Int64, ENV["LSB_JOBINDEX"])
-idx = 3
 filepath = joinpath.("dataset/train/", readdir("dataset/train/"))[idx]
 filename = split(split(filepath, ".")[1],"/")[3]
 
