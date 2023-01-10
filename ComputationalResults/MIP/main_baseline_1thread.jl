d5ae0228d989b41262efca93ba89ef56e6c9efa8
thearonn <thea.ronn@hotmail.com>
2023-01-10 21:41:14 +0100

    hej

--- ComputationalResults/MIP/main_baseline_1thread.jl
+++ ComputationalResults/MIP/main_baseline_1thread.jl
@@ -2,16 +2,14 @@ include("../../ReadWrite.jl")
 include("../../MIPModels.jl")
 include("../../BasicFunctions.jl")
 
-#idx = parse(Int64, ENV["LSB_JOBINDEX"])
-idx = 4
+idx = parse(Int64, ENV["LSB_JOBINDEX"])
+
 filepath = joinpath.("dataset/test/", readdir("dataset/test/"))[idx]
 filename = split(split(filepath, ".")[1],"/")[3]
 folder = "ComputationalResults/MIP/results/"
 data = readInstance(filepath)
 
 timelimit = 30*60
-timelimit = 30
-
 
 
 x, gap, time = MIPBaseline(data, "Gurobi", 1, timelimit, 0, 1)
