a7e2d7083706b05ee7cb4668f0a24dfb01a1c797
sofiebach <70587917+sofiebach@users.noreply.github.com>
2023-01-14 18:30:36 +0100

    new results

--- ComputationalResults/TimeObjective/time_table.jl
+++ ComputationalResults/TimeObjective/time_table.jl
@@ -4,18 +4,18 @@ using PyCall
 
 py"""
 import matplotlib.pyplot as plt 
-def timeplot(time_limits, MIPobjs, ALNSobjs, title, filename):
+def timeplot(time_limits_MIP, time_limits_ALNS, MIPobjs, ALNSobjs, title, filename):
     plt.figure()
-    plt.plot(time_limits, MIPobjs,'o',color='tab:blue',markersize = 6)
-    plt.plot(time_limits, ALNSobjs,'o',color='darkorange',markersize = 6)
-    plt.plot(time_limits, MIPobjs,'-',color='tab:blue')
-    plt.plot(time_limits, ALNSobjs,'-',color='darkorange')
+    plt.plot(time_limits_MIP, MIPobjs,'o',color='tab:blue',markersize = 6)
+    plt.plot(time_limits_ALNS, ALNSobjs,'o',color='darkorange',markersize = 6)
+    plt.plot(time_limits_MIP, MIPobjs,'-',color='tab:blue')
+    plt.plot(time_limits_ALNS, ALNSobjs,'-',color='darkorange')
     plt.xlabel("Time (s)",fontsize="15")
     plt.ylabel("Objective value",fontsize="15")
     plt.title(title, fontsize="15")
     plt.tick_params(axis='x', labelsize=15)
     plt.tick_params(axis='y', labelsize=15)
-    plt.legend(["Solver", "ALNS"], fontsize="15")
+    plt.legend(["Solver", "ALNS"], fontsize="15", loc='upper right')
     #plt.show()
     plt.savefig(filename, bbox_inches='tight')
     plt.close()
@@ -33,6 +33,8 @@ write(ALNS_table, "& \\multicolumn{6}{c}{\\textbf{Time (s)}} \\\\ \n")
 counter = 0
 sum_objs_MIP = zeros(Float64, 6)
 sum_objs_ALNS = zeros(Float64, 6)
+count_MIP = zeros(Float64, 6)
+count_ALNS = zeros(Float64, 6)
 for file in filepath 
     counter += 1
     name = split(file, "/")[end]
@@ -51,8 +53,16 @@ for file in filepath
     ALNS_avg_objs = parse.(Float64,split(readline(f)))
     close(f)
 
-    sum_objs_MIP += MIP_objs
-    sum_objs_ALNS += ALNS_avg_objs
+    for i = 1:length(time_limits)
+        if MIP_objs[i] < 100
+            sum_objs_MIP[i] += MIP_objs[i]
+            count_MIP[i] += 1
+        end
+        if ALNS_avg_objs[i] < 100
+            sum_objs_ALNS[i] += ALNS_avg_objs[i]
+            count_ALNS[i] += 1
+        end
+    end
 
     write(MIP_table, "\\texttt{"*replace(name, "_" => "\\_")*"} & "*join(MIP_objs, " & "))
     write(MIP_table, "\\\\ \n")
@@ -61,9 +71,9 @@ for file in filepath
 
     if counter % 3 == 0
         title = split(name, "_")[1] * "-instances"
-        avg_objs_MIP = round.(sum_objs_MIP/3, digits=2)
-        avg_objs_ALNS = round.(sum_objs_ALNS/3, digits=2)
-        py"timeplot"(time_limits, avg_objs_MIP, avg_objs_ALNS, title, title*".pdf")
+        avg_objs_MIP = round.(sum_objs_MIP./count_MIP, digits=2)
+        avg_objs_ALNS = round.(sum_objs_ALNS./count_ALNS, digits=2)
+        py"timeplot"(time_limits[avg_objs_MIP .< 100], time_limits[avg_objs_ALNS .< 100], avg_objs_MIP[avg_objs_MIP .< 100], avg_objs_ALNS[avg_objs_ALNS .< 100], title, title*".pdf")
         write(MIP_table, "\\midrule \n")
         write(MIP_table, "Average & "*join(avg_objs_MIP, " & ")*"\\\\ \n")
         write(MIP_table, "\\midrule \n")
@@ -72,6 +82,8 @@ for file in filepath
         write(ALNS_table, "\\midrule \n")
         sum_objs_MIP = zeros(Float64, length(time_limits))
         sum_objs_ALNS = zeros(Float64, length(time_limits))
+        count_MIP = zeros(Float64, length(time_limits))
+        count_ALNS = zeros(Float64, length(time_limits))
     end
 end
 close(MIP_table)
